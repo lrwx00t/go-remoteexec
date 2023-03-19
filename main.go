006915d4806915fca5fd7640e14e17af541c045f
lrwx00t <lrwx00t@gmail.com>
2023-03-19 15:30:15 -0400

    refactor

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"crypto/x509"
-	"encoding/pem"
-	"errors"
 	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 
+	"github.com/lrwx00t/go-remoteexec/ssh_utils"
 	"golang.org/x/crypto/ssh"
 )
 
@@ -48,7 +46,8 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		signer, err := signerFromPem(pemBytes, []byte(""))
+
+		signer, err := ssh_utils.SignerFromPem(pemBytes, []byte(""))
 		if err != nil {
 			panic(err)
 		}
@@ -76,69 +75,6 @@ func main() {
 	fmt.Println("finished boostrap. cleaning up..")
 	session_execute(delete_repo, conn)
 
-}
-
-func signerFromPem(pemBytes []byte, password []byte) (ssh.Signer, error) {
-	err := errors.New("pem decode failed, no key found")
-	pemBlock, _ := pem.Decode(pemBytes)
-	if pemBlock == nil {
-		return nil, err
-	}
-
-	if x509.IsEncryptedPEMBlock(pemBlock) {
-		pemBlock.Bytes, err = x509.DecryptPEMBlock(pemBlock, []byte(password))
-		if err != nil {
-			return nil, fmt.Errorf("decrypting PEM block failed %v", err)
-		}
-
-		key, err := parsePemBlock(pemBlock)
-		if err != nil {
-			return nil, err
-		}
-
-		signer, err := ssh.NewSignerFromKey(key)
-		if err != nil {
-			return nil, fmt.Errorf("creating signer from encrypted key failed %v", err)
-		}
-
-		return signer, nil
-	} else {
-		// generate signer instance from plain key
-		signer, err := ssh.ParsePrivateKey(pemBytes)
-		if err != nil {
-			return nil, fmt.Errorf("parsing plain private key failed %v", err)
-		}
-
-		return signer, nil
-	}
-}
-
-func parsePemBlock(block *pem.Block) (interface{}, error) {
-	switch block.Type {
-	case "RSA PRIVATE KEY":
-		key, err := x509.ParsePKCS1PrivateKey(block.Bytes)
-		if err != nil {
-			return nil, fmt.Errorf("parsing PKCS private key failed %v", err)
-		} else {
-			return key, nil
-		}
-	case "EC PRIVATE KEY":
-		key, err := x509.ParseECPrivateKey(block.Bytes)
-		if err != nil {
-			return nil, fmt.Errorf("parsing EC private key failed %v", err)
-		} else {
-			return key, nil
-		}
-	case "DSA PRIVATE KEY":
-		key, err := ssh.ParseDSAPrivateKey(block.Bytes)
-		if err != nil {
-			return nil, fmt.Errorf("parsing DSA private key failed %v", err)
-		} else {
-			return key, nil
-		}
-	default:
-		return nil, fmt.Errorf("parsing private key failed, unsupported key type %q", block.Type)
-	}
 }
 
 func session_execute(cmd string, conn *ssh.Client) {
